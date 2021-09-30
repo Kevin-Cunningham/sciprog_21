781a42de4f56d35368bcd58b5ad79f404f275a15
Kevin-Cunningham <kevincunningham123@gmail.com>
2021-09-30 15:31:19 +0100

    Added a loop to perform sum over domain

--- practical3/Integration.c
+++ practical3/Integration.c
@@ -5,10 +5,20 @@ int main(void) {
 
 	/*Initialise variables*/
 	int i=0, N=12;
-	double a=0, b=M_PI/3.0, sum=0.0;
+	double a=0, b=M_PI/3.0;
 	
 	/*Define width of trapezoids*/
 	double update = (b-a)/N;	
+	
+	/*Initialise sum*/
+	double sum = tan(a)+tan(b);
+	
+	/*Sum from x_1 to x_11*/
+	for(i=1, i<N ,i++){
+	   sum += 2*tan(i*update)	
+	}
 
+	/*Multiply sum by (1/2)update */
+	double answer = ((double) 0.5)*update*sum
 
 }
