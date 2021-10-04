fea663e638420990c78b79dae2297f9c458bf889
Kevin-Cunningham <kevincunningham123@gmail.com>
2021-10-04 18:57:47 +0100

    add and test degtorad which coneverts angles in degrees to angles in radians

--- practical4/exercise4.c
+++ practical4/exercise4.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <math.h>
 
+float degtorad(float x);
 float xdeg[13], xrad[13], pi = M_PI;
 
 
 int main(void)	{
+	printf("180 degrees is %f radians \n",degtorad(180));
 
+}
 
+float degtorad(float x){
+	return x*pi/180;
 }
