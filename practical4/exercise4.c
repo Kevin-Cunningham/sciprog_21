be5c92827b5cc64b7537dd37a278058f02b1a594
Kevin-Cunningham <kevincunningham123@gmail.com>
2021-10-04 19:56:48 +0100

    needed to change stepsize to radians to give correct area under curve (Assuming the answer is supposed to that of practical 3), also added comments

--- practical4/exercise4.c
+++ practical4/exercise4.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <math.h>
 
+/*deg to rad converts degrees to radians*/
+/*trap using the trapezoidal rule to integrate from 0 degrees to 60 degrees in N columns*/
+/*results holdsthe results of tan(x) as x varies from 0 deg to 60 deg  in steps of 5 deg*/
 double degtorad(double x);
 double trap(int numPoints);
 double results[13], pi = M_PI;
@@ -9,10 +12,12 @@ double results[13], pi = M_PI;
 int main(void)	{
 	int i;
 	for(i=0; i<13; i++){
+		/*assign each entry of results*/
 		results[i] = tan( degtorad(  ((double) 5.0)*i  ) );
+		/*print each entry as it's added to be checked*/	
 		printf("%lf \n",results[i]);
 	}
-
+	/*print the result of trapezoidal integration*/
 	printf("The result of integration is %lf \n",trap((int) 12));
 }
 
@@ -21,15 +26,22 @@ double degtorad(double x){
 }
 
 double trap(int numPoints){
+	/*area will be the final result*/
+	/*sum will be the height of each point being included*/
+	/*stepsize is the distance between points*/
 	double area=0, sum=0, stepsize = (60.0 - 0.0)/numPoints;
 	
+	/*initialise sum with the endpoints as in practical 3*/
 	sum = tan( degtorad((double) 0) ) + tan( degtorad ((double) 60) );
 	
+	/*update sum to include intermediate points */
 	int j;
 	for(j=1; j<numPoints; j++){
-		sum += tan(degtorad( j*stepsize ));
+		sum +=((int) 2)*tan(degtorad( j*stepsize ));
 	}	
-	area = ((double) .5)*stepsize*sum;
+	/*multiply sum by width for final result*/
+	/*note stepsize must be changed from degs to rads*/
+	area = ((double) .5)*degtorad( stepsize )*sum;
 	return area;
 }
 
