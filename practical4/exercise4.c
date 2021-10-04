98e91154fc134b93da1af3de1a0eec6dfca004e4
Kevin-Cunningham <kevincunningham123@gmail.com>
2021-10-04 19:06:39 +0100

    initialise and check arrays containing angles in degrees and radians

--- practical4/exercise4.c
+++ practical4/exercise4.c
@@ -6,7 +6,15 @@ float xdeg[13], xrad[13], pi = M_PI;
 
 
 int main(void)	{
-	printf("180 degrees is %f radians \n",degtorad(180));
+	int i, j;
+	for(i=0; i<13; i++){
+		xdeg[i] = i*((float) 5);
+		printf("%f \n",xdeg[i]);
+	}
+	for(j=0; j<13; j++){
+		xrad[j] = degtorad( xdeg[j] );
+		printf("%f \n", xrad[j]);
+	}
 
 }
 
