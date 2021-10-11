393104595dc2ff6e73e81614683c08400c746fec
Kevin-Cunningham <kevincunningham123@gmail.com>
2021-10-11 17:11:24 +0100

    added myAbs which returns the abolute value of it's argument. This is used to fix artanhl, which previously did not work for negative numbers.

--- practical5/practical5part2.c
+++ practical5/practical5part2.c
@@ -5,29 +5,39 @@ int main(void){
 
 	double artanhl(double x, double delta);
 	double artanh2(double x);
-	double x[181];
+	double MacLaurin[181], LogApprox[181];
 	double delta;
 	int i, j;
-	for (i=0; i<=180; i++){
-		x[i] = ((double) .01)*i -((double) .9);
-	}
-	for (j=0; j<=180; j++){
-	printf("n=%d, x=%lf\n",j,x[j]);
 
-	}
 	printf("Enter the required tolerance.\n");
 	scanf("%lf", &delta);
+
 	
+	for (j=0; j<=180; j++){
+		MacLaurin[j]=artanhl( ((double) .01)*j - ((double) .9)  , delta );
+		LogApprox[j]=artanh2( ((double) .01)*j - ((double) .9) );
+		printf("n=%d, x=%lf, MacLaurin=%lf, Log approximation=%lf\n", j, ((double) .01)*j-((double) .9), MacLaurin[j], LogApprox[j]);
+	}
+	
+	
+}
+
+double myAbs(double y){
+	if(y>=0){
+		return y;}
+	else{
+		return -y;}	
 }
 
 
 double artanhl(double x, double delta){
+	double myAbs(double y);
 	int n = 0;
 	double sum=0.0;
 	/*First term in the series is x*/
 	double term = x;
 	
-	while(term>delta){
+	while(myAbs(term)>delta){
 		sum += term;
 		n+=1;
 		term = (pow(x,((double) (2*n + 1))))/((double) (2*n+1));
