455355dc88fa9e1056cbefb2360c1be2b0072cd4
Kevin-Cunningham <kevincunningham123@gmail.com>
2021-10-11 13:55:54 +0100

    added the approximation of the McLaurin series. Script asks for x value and tolerance, then prints result.

--- practical5/practical5part2.c
+++ practical5/practical5part2.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
+#include <math.h>
 int main(void){
+	double artanhl(double x, double delta);
+	double x;
 	double delta;
-	printf("Enter the required tolerance.\n");
-	scanf("%lf", &delta);
-
+	printf("Enter the value x, and required tolerance.\n");
+	scanf("%lf %lf", &x, &delta);
+	printf("arctanh is %lf \n", artanhl(x,delta));
 }
-
+double artanhl(double x, double delta){
+	int n = 0;
+	double sum=0.0;
+	/*First term in the series is x*/
+	double term = x;
+	
+	while(term>delta){
+		sum += term;
+		n+=1;
+		term = (pow(x,((float) (2*n + 1))))/((float) (2*n+1));
+	}
+	return sum;
+}
