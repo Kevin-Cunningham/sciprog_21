e231b85d19fc9a801fb8750d68bf2640ef921670
Kevin-Cunningham <kevincunningham123@gmail.com>
2021-10-11 14:01:18 +0100

    Added the log approximation.

--- practical5/practical5part2.c
+++ practical5/practical5part2.c
@@ -2,11 +2,12 @@
 #include <math.h>
 int main(void){
 	double artanhl(double x, double delta);
+	double artanh2(double x);
 	double x;
 	double delta;
 	printf("Enter the value x, and required tolerance.\n");
 	scanf("%lf %lf", &x, &delta);
-	printf("arctanh is %lf \n", artanhl(x,delta));
+	printf("The approximation to arctanh is %lf, while the log approximation is %lf \n", artanhl(x,delta), artanh2(x));
 }
 double artanhl(double x, double delta){
 	int n = 0;
@@ -17,7 +18,10 @@ double artanhl(double x, double delta){
 	while(term>delta){
 		sum += term;
 		n+=1;
-		term = (pow(x,((float) (2*n + 1))))/((float) (2*n+1));
+		term = (pow(x,((double) (2*n + 1))))/((double) (2*n+1));
 	}
 	return sum;
+}
+double artanh2(double x){
+	return ((double) 0.5)*(log(1+x)-log(1-x));
 }
