cdace2e609b909383584d6f5f21b8c3b10947c08
Kevin-Cunningham <kevincunningham123@gmail.com>
2021-10-11 13:05:12 +0100

    initialised an array to store Fibonacci sequence. Fill it with dummy entries to check it works.

--- practical5/practical5.c
+++ practical5/practical5.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 int main(void){
 	/*n to be scanned in, i is an index for the later loop*/
-	int n, i;
+	int n, i, j;
 	printf("Enter an integer n, and I will calculate the n-th value of the Fibonacci series.\n");
 	scanf("%d",&n);
 	printf("You entered %d.\n", n);
 
+	int FibSeries[n+1];
+	FibSeries[0]=0;
+	FibSeries[1]=1;
+	for (i=2; i<=n; i++){
 
+		FibSeries[i]= i;
+	}
+	for (j=0; j<=n; j++){
+		printf("%d \n",FibSeries[j]);
+	}
 }
