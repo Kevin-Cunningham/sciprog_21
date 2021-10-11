5225920f5ed18bc728471f0c62c403a8f10438b3
Kevin-Cunningham <kevincunningham123@gmail.com>
2021-10-11 12:48:42 +0100

    Initial commit. Includes code for to scan in the integer n, and print it to check it.

---
+++ practical5/practical5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void){
+	/*n to be scanned in, i is an index for the later loop*/
+	int n, i;
+	printf("Enter an integer n, and I will calculate the n-th value of the Fibonacci series.\n");
+	scanf("%d",&n);
+	printf("You entered %d.\n", n);
+
+
+}
