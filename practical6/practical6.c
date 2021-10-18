ea6f501aad11cb815ad3d529d888bf9ec95791af
Kevin-Cunningham <kevincunningham123@gmail.com>
2021-10-18 11:57:13 +0100

    All matrices intialised. C caluclated, matrices printed out

--- practical6/practical6.c
+++ practical6/practical6.c
@@ -1,13 +1,59 @@
 #include <stdio.h>
 
 int main(void){
-	int n=5, p=3, q=4, i, j;
-	double A[n][p], B[p][q], C[n][q];
+	int n=5, p=3, q=4, i, j, k;
+	double A[n][p], B[p][q], C[n][q],temp=0.0;
 	
 	for(i=0; i<n; i++){
-		for(j=0; j<p; j++){
+		for(j=0; j<q; j++){
 			C[i][j]=0.0;
-			printf("%d, %d, %lf\n",i,j,C[i][j]);
 		}
 	}
+
+	for(i=0; i<n; i++){
+		for(j=0; j<p; j++){
+			A[i][j]=i+j;
+		}
+	}
+
+	for(i=0; i<p; i++){
+		for(j=0; j<q; j++){
+			B[i][j]=i-j;
+		}
+	}
+
+	for(i=0; i<n; i++){
+		for(j=0; j<q; j++){
+			temp=0.0;
+			for(k=0; k<p; k++){
+				temp += A[i][k]*B[k][j];
+			}
+			C[i][j]= temp;
+		}
+	} 
+
+	printf("Print out of A:\n");
+	for(i=0; i<n; i++){
+		for(j=0; j<p; j++){
+			printf("%lf ", A[i][j]);
+		}
+		printf("\n");
+	}
+
+	printf("Print out of B: \n");
+	for(i=0; i<p; i++){
+		for(j=0; j<q; j++){
+			printf("%lf ", B[i][j]);
+		}
+		printf("\n");
+	}
+
+	printf("Print out of C: \n");
+	for(i=0; i<n; i++){
+		for(j=0; j<q; j++){
+			printf("%lf ", C[i][j]);
+		}
+		printf("\n");
+	}
+	
 }
