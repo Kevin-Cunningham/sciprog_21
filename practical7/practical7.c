4a211536f961b804c82e503e4ac8903f1bf02096
Kevin-Cunningham <kevincunningham123@gmail.com>
2021-10-28 15:04:11 +0100

    Added and tested printarray function

--- practical7/practical7.c
+++ practical7/practical7.c
@@ -5,15 +5,14 @@
 void main(){
 	int *allocatearray(int n);
 	void fillwithones(int *p, int n);
+	void printarray(int *p, int n);
 	int i, j,*r;
 
 	r = allocatearray(5);
 		
 	fillwithones(r,5);
 
-	for (j=0;j<5;j++){
-		printf("r[%d] = %d\n",j, r[j]);
-	}
+	printarray(r,5);
 
 }
 
@@ -23,9 +22,17 @@ int *allocatearray(int n){
 	return alloc;
 }
 
+//Fill with ones needs the length of the array, as sizeof won't work on a dynamically allocated array
 void fillwithones(int *p, int n){
 	int i;
 	for(i=0; i<n; i++){
 		p[i]=1;
+	}
+}
+
+void printarray(int *p, int n){
+	int i;
+	for(i=0; i<n; i++){
+		printf("%d\n", p[i]);
 	}
 }
