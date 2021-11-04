0ac3ebfdde7a084c905f89e5c08fbaf3df2d51f4
Kevin-Cunningham <kevincunningham123@gmail.com>
2021-11-04 16:33:13 +0000

    added more commenting

--- practical8/iterate.c
+++ practical8/iterate.c
@@ -1,10 +1,12 @@
+//compile as
+//gcc -o iter iterate.c
 #include <stdio.h>
-#include <math.h>
 
 void main(){
+	//Initialise function in main block
 	int GCD(int a, int b);
 	
-	
+	//Print a few examples
 	printf("The GCD of 8 and 12 is %d\n", GCD(8,12));
 	printf("The GCD of 18 and 27 is %d\n", GCD(18,27));
 	printf("The GCD of 52 and 91 is %d\n", GCD(52, 91));
@@ -12,6 +14,7 @@ void main(){
 }
 
 int GCD(int a, int b){
+	//The function from the pseudocode on the practical sheet
 	int temp;
 	while(b!=0){
 		temp = b;
