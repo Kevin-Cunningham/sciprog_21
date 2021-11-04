80643de52369946a27b3fde7ffb7f943909b4ab9
Kevin-Cunningham <kevincunningham123@gmail.com>
2021-11-04 16:33:00 +0000

    added more commenting

--- practical8/recurse.c
+++ practical8/recurse.c
@@ -1,13 +1,18 @@
+//compile as
+//gcc -o recu recurse.c
 #include <stdio.h>
-	int GCD(int a, int b);
+//intialise function
+int GCD(int a, int b);
 
 void main(){
+	//Print out a few examples
 	printf("The GCD of 8 and 12 is %d\n",  GCD(8,12));
 	printf("The GCD of 18 and 27 is %d\n", GCD(18,27));
 	printf("The GCD of 52 and 91 is %d\n", GCD(52,91));
 }
 
 int GCD(int a, int b){
+	//Recursive function code as layed out on practical sheet
 	if(b!=0){
 		return GCD(b, a%b);
 	}
