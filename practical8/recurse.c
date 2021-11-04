af27c878ae7bb24fec61e1961147caeaf80ee05a
Kevin-Cunningham <kevincunningham123@gmail.com>
2021-11-04 16:28:28 +0000

    Added recursive function

--- practical8/recurse.c
+++ practical8/recurse.c
@@ -3,15 +3,15 @@
 
 void main(){
 	printf("The GCD of 8 and 12 is %d\n",  GCD(8,12));
-	//printf("The GCD of 18 and 27 is %d\n", GCD(18,27));
-	//printf("The GCD of 52 and 91 is %d\n", GCD(52,91));
+	printf("The GCD of 18 and 27 is %d\n", GCD(18,27));
+	printf("The GCD of 52 and 91 is %d\n", GCD(52,91));
 }
 
 int GCD(int a, int b){
-	if (b=0){
-		return a;
-	}
-	else if(b!=0){
+	if(b!=0){
 		return GCD(b, a%b);
+	}
+	else if (b=0){
+		return a;
 	}	
 }
