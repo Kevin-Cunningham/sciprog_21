ff2af3c26935f7d800f31e7eb08c21c5d3eca194
Kevin-Cunningham <kevincunningham123@gmail.com>
2021-11-08 13:50:48 +0000

    Initial commit. Not part of practical. This script makes a matrix and saves it in a .txt file

---
+++ practical9/makeMatrix.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+
+void main(){
+ 	FILE *f;
+	//n is the size of the square matrix, i,j are indexing variables, temp is a temporary variable 
+	int n,i,j,temp;
+	
+	//Get the size of the square matrix
+	printf("Size (n) of square matrix:\n");
+	scanf("%d",&n);
+	
+	//intialise square matrix of size n
+	int Mat[n][n];
+	
+	//Take in values of square matrix
+	for (i=0; i<n; i++){
+		for (j=0; j<n; j++){
+			printf("Enter the integer value for row %d, column %d \n", i+1, j+1);
+			scanf("%d", &temp);
+			Mat[i][j]=temp;
+		}
+
+	}
+	//Prints out square matrix to be checked
+	for(i=0; i<n; i++){
+		for(j=0;j<n;j++){
+			printf("%d  ",Mat[i][j]);
+		}
+		printf("\n");
+	}	
+	
+	//open file
+ 	f = fopen("matrix1.txt","w");
+       	
+	//print matrix size to file
+	fprintf(f, "%d ", n);
+	
+	//Add matrix to file as a sequence of numbers separated by 
+	for(i=0;i<n;i++){
+		for(j=0;j<n;j++){
+			fprintf(f,"%d,",Mat[i][j]);
+		}
+	}
+
+	fclose(f);	
+
+}
+
