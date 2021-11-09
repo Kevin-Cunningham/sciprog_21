ca61e56e3f0b2ff202cd3c72409bdf885a45848d
Kevin-Cunningham <kevincunningham123@gmail.com>
2021-11-09 10:08:43 +0000

    Added code to create pointer to pointer to int instead of array of arrays.

--- practical9/main.c
+++ practical9/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "magic_square.h"
 
 void main(){
 
@@ -15,25 +16,46 @@ void main(){
 	exit(1);
 	}	
 
-	fscanf(f, "%d", &size);
+	fscanf(f, "%d ", &size);
 	printf("The matrix is %d x %d\n", size, size);
-	
+		
 	int matrix[size][size]; 
 	
 	for(i=0;i<size;i++){
 		for(j=0; j<size;j++){
-		fscanf(f,delim,&matrix[i][j]);
+			fscanf(f,delim,&matrix[i][j]);
 		}
 	}
 
 	fclose(f);
 	
+	
+
 	printf("The matrix looks like:\n");
 	for (i=0; i<size; i++){
 		for (j=0;j<size;j++){
-		printf("%d  ",matrix[i][j]);
+			printf("%d ",matrix[i][j]);
 		}
 		printf("\n");
 	}
+
+	int* rowpoint[size]; 
+	for(i=0;i<size;i++){
+		rowpoint[i] = &matrix[i][0];
+	}
+  	int **matpoint;
+	matpoint = &rowpoint[0];
+	printf("\nCheck that pointer to matrix works\n");
+	for(i=0;i<size;i++){
+		for (j=0;j<size;j++){
+			printf("%d ",matpoint[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+	int Bool;
+        //Bool = isMagicSquare(matrix, size);
+	//printf("%d\n",Bool);
 	
+
 }
