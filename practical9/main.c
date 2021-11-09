1296da00b759d1b82756791acd6110512dead501
Kevin-Cunningham <kevincunningham123@gmail.com>
2021-11-09 11:04:19 +0000

    Improved commenting

--- practical9/main.c
+++ practical9/main.c
@@ -4,25 +4,32 @@
 #include "magic_square.h"
 
 void main(){
-
+	//f will hold the file that the matrix is stored in
 	FILE *f;
+	//Size is the size of the square matrix, e.g. size=3 => 3x3 matrix
+	//i,j are inidces
 	int size, i,j;
 	char delim[] = "%d ", filename[20];
+	
+	//Ask the User what file they want to check
 	printf("What file contains the matrix you want to check?\n");
 	scanf("%s",filename);
 	strcat(filename,".txt");
 	f = fopen(filename,"r");
-
+	
+	//Error statement in case the file cant be opened
 	if (f==NULL){
 	printf("!! Can't open file!!\n");
 	exit(1);
 	}	
-
+	
+	//Read in the size of the matrix
 	fscanf(f, "%d ", &size);
-	printf("The matrix is %d x %d\n", size, size);
+	printf("\nThe matrix is %d x %d\n", size, size);
 		
 	int matrix[size][size]; 
 	
+	//Create the matrix in an array
 	for(i=0;i<size;i++){
 		for(j=0; j<size;j++){
 			fscanf(f,delim,&matrix[i][j]);
@@ -32,31 +39,35 @@ void main(){
 	fclose(f);
 	
 	
-
-	printf("The matrix looks like:\n");
+	//Print the matrix so the user can check that it is the correct one
+	printf("The matrix looks like:\n\n");
 	for (i=0; i<size; i++){
 		for (j=0;j<size;j++){
 			printf("%d ",matrix[i][j]);
 		}
 		printf("\n");
 	}
-
+	
+	//Create an array of pointers which each point to a row in the matrix
 	int* rowpoint[size]; 
 	for(i=0;i<size;i++){
 		rowpoint[i] = &matrix[i][0];
 	}
+	//Create a pointer to pointers in the previous array
   	int **matpoint;
 	matpoint = &rowpoint[0];
-	printf("\nCheck that pointer to matrix works\n");
-	for(i=0;i<size;i++){
-		for (j=0;j<size;j++){
-			printf("%d ",matpoint[i][j]);
-		}
-		printf("\n");
-	}
+	//printf("\nCheck that pointer to matrix works\n");
+	//for(i=0;i<size;i++){
+	//	for (j=0;j<size;j++){
+	//		printf("%d ",matpoint[i][j]);
+	//	}
+	//	printf("\n");
+	//}
 	printf("\n");
 	int Bool;
-        Bool = isMagicSquare(matpoint, size);
+        //Call the isMagicSquare function on our matrix
+	Bool = isMagicSquare(matpoint, size);
+	//If the function returns True (1), then the code prints that the matrix is magic, otherwise it prints that it is not magic
 	if (Bool == 1){
 		printf("The matrix IS magic!\n");
 	}
