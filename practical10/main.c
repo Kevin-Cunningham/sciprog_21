e59771649cc3297f0385a401656b493741d2366b
Kevin-Cunningham <kevincunningham123@gmail.com>
2021-11-18 12:17:00 +0000

    Initial commit. includes function working for n=1.

---
+++ practical10/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void sort(int n);
+
+void main(){
+	int k;
+	printf("What is the size of the stack?\n");
+	scanf("%d", &k);
+	sort(k);
+
+	printf("\n");
+}
+
+void sort(int n){
+	
+	if(n==1){
+		printf("1 -> 3\n");
+	}
+	else{
+		printf("TBD\n");
+	}
+}
+
