0e406b95d4980cb06b730e0cd443292502d3fc97
Kevin-Cunningham <kevincunningham123@gmail.com>
2021-11-18 12:50:20 +0000

    Added recursive part to do n>1

--- practical10/main.c
+++ practical10/main.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 
-void sort(int n);
+void sort(int n, int start, int inter, int end);
 
 void main(){
 	int k;
 	printf("What is the size of the stack?\n");
 	scanf("%d", &k);
-	sort(k);
+	sort(k,1,2,3);
 
 	printf("\n");
 }
 
-void sort(int n){
+void sort(int n, int start, int inter, int end){
 	
 	if(n==1){
-		printf("1 -> 3\n");
+		printf("%d -> %d\n", start, end);
 	}
 	else{
-		printf("TBD\n");
+		sort(n-1,start, end, inter);
+		printf("%d -> %d\n",start, end);
+		sort(n-1,inter, start, end);
 	}
 }
 
